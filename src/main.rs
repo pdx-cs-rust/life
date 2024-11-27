68879655baa8d4ce07853e590f13a1b3c7621e0a
Bart Massey <bart.massey@gmail.com>
2024-11-26 19:46:28 -0800

    made display stretch to match window size

--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,28 @@ use ggez::{self, error, event, graphics};
 // Code inspired by
 // http://github.com/ironwall/rust-conway_life_game
 
-const WORLD_SIZE: (usize, usize) = (200, 300);
-const CELL_SIZE: f32 = 2.0;
-const SCREEN_SIZE: (f32, f32) =
-    (WORLD_SIZE.1 as f32 * CELL_SIZE, WORLD_SIZE.0 as f32 * CELL_SIZE);
-
 struct MainState {
     world: World,
+    screen_coordinates: graphics::Rect,
+    cell_size: f32,
+    world_size: (usize, usize),
 }
 
 impl MainState {
     fn new() -> ggez::GameResult<MainState> {
-        let s = MainState { world: World::random(WORLD_SIZE) };
+        let world_size = (300, 200);
+        let cell_size = 2.0;
+        let s = MainState {
+            world: World::random((world_size.1, world_size.0)),
+            screen_coordinates: graphics::Rect::new(
+                0.0,
+                0.0,
+                world_size.0 as f32 * cell_size,
+                world_size.1 as f32 * cell_size,
+            ),
+            cell_size,
+            world_size,
+        };
         Ok(s)
     }
 }
@@ -45,35 +55,57 @@ impl event::EventHandler<error::GameError> for MainState {
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
         let mut canvas = graphics::Canvas::from_frame(
             ctx,
-            Some([0.0, 0.0, 0.0, 1.0].into()),
+            graphics::Color::BLACK,
         );
 
+        let cs = self.cell_size;
         let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
-            [0.5 * CELL_SIZE, 0.5 * CELL_SIZE],
-            0.6 * CELL_SIZE,
-            0.5,
+            [0.5 * cs, 0.5 * cs],
+            0.6 * cs,
+            0.25,
             graphics::Color::WHITE,
         )?;
 
         for (r, c, alive) in self.world.cells() {
             if alive {
                 let coord =
-                    [c as f32 * CELL_SIZE, r as f32 * CELL_SIZE];
+                    [c as f32 * cs, r as f32 * cs];
                 canvas.draw(&circle, coord);
             }
         }
 
         canvas.finish(ctx)
     }
+
+    // Adapted from `ggez/examples/graphics_settings.rs`.
+    fn resize_event(
+        &mut self,
+        _ctx: &mut ggez::Context,
+        width: f32,
+        height: f32,
+    ) -> ggez::GameResult {
+        self.cell_size = f32::min(
+            width / self.world_size.0 as f32,
+            height / self.world_size.1 as f32,
+        );
+        self.screen_coordinates =
+            graphics::Rect::new(0.0, 0.0, width * self.cell_size, height * self.cell_size);
+        Ok(())
+    }
 }
 
 pub fn main() -> ggez::GameResult {
+    let state = MainState::new()?;
+
     let cb = ggez::ContextBuilder::new("Life", "PSU CS 410/510 Rust Su2019");
     let setup = ggez::conf::WindowSetup::default();
     let mode = ggez::conf::WindowMode::default()
-        .dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1)
+        .dimensions(
+            state.world_size.0 as f32 * state.cell_size,
+            state.world_size.1 as f32 * state.cell_size,
+        )
         .resizable(true);
 
     let (ctx, event_loop) = cb
@@ -81,6 +113,5 @@ pub fn main() -> ggez::GameResult {
         .window_mode(mode)
         .build()?;
 
-    let state = MainState::new()?;
     event::run(ctx, event_loop, state)
 }
